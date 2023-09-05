868ecc85aa0b65eb45a850008ba0b6ab1fa0c2e9
kevin stal <kevin.stal@gmx.ch>
2023-09-05 11:54:27 +0200

    workout user stories

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,31 @@
 class WorkoutsController < ApplicationController
+
+ # before_action :authenticate_user!
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+    @exercises = @workout.exercises
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout, notice: 'Workout was successfully created!'
+    else
+      render :new
+    end
+  end
 end
+
+  private
+
+    def workout_params
+      params.require(:workout).permit(:name, :date, :duration, exercise_ids: [])
+    end
