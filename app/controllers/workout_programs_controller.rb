3296e7e6e018e607d78b2930792d1d8c811741e1
Ayub Hassan <ayyubahmed555@gmail.com>
2023-09-05 10:00:04 +0100

    Creating workout_program story

--- app/controllers/workout_programs_controller.rb
+++ app/controllers/workout_programs_controller.rb
@@ -1,2 +1,36 @@
 class WorkoutProgramsController < ApplicationController
+    before_action :find_workout_program, only: [:show, :edit, :update]
+    def show
+    end
+    
+    def edit
+    end
+    def update
+        if @workout_program.update(workout_program_params)
+          redirect_to workout_program_path(@workout_program), notice: "Successfully updated your workout program"
+        else
+          render 'edit', status: :unprocessable_entity
+        end
+    def new
+        @workout_programs = WorkoutProgram.new
+    end
+     
+    def create
+        @workout_programs = WorkoutProgram.new(workout_program_params)
+        if @workout_programs.save
+            redirect_to   new_workout_program_workout_path notice: "You have successfully created your workout program"
+        else 
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private 
+    def workout_program_params
+        params.require(:workout_program).permit(:total_duration, :weekly_frequency, :workout_split, :week, :start_date, :end_date)
+    end
+
+    def find_workout_program
+        @workout_program = WorkoutProgram.find(params[:id])
+    end
+
 end
