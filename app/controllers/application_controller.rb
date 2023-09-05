4134ae1609d0101602b0ad37399de165a2db423f
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-09-05 09:14:44 +0000

    workout program authorization done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  include Pundit::Authorization
+
+  # Pundit: allow-list approach
+  after_action :verify_authorized, unless: :skip_pundit?
+  after_action :very_policy_scoped, unless: :skip_pundit? # Don't know what this line does exactly
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
