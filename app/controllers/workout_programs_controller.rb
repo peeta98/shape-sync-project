1a63d948a7f6b7156e53f95ae7f1c91f47385248
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2023-09-08 14:50:13 +0200

    completed the workout

--- app/controllers/workout_programs_controller.rb
+++ app/controllers/workout_programs_controller.rb
@@ -49,7 +49,7 @@ class WorkoutProgramsController < ApplicationController
   private
 
   def workout_program_params
-    params.require(:workout_program).permit(:weekly_frequency, :start_date, :end_date, workouts_attributes: [:id, :date, :categories, :duration, :_destroy])
+    params.require(:workout_program).permit(:weekly_frequency, workouts_attributes: [:id, :date, :categories, :duration, :_destroy])
   end
 
   def find_workout_program
